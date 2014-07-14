f18d4951add73b00c38414308c377d593528a43b
Daniel Arnould <dan@arnould.co.uk>
2014-07-14 20:26:04 +0100

    Initial

---
+++ lib/github_whitespace_demo.rb
@@ -0,0 +1,9 @@
+require "github_whitespace_demo/version"
+
+module GithubWhitespaceDemo
+  class Klass
+    def initialize
+      @property = "test"
+    end
+  end
+end
---
+++ lib/github_whitespace_demo/version.rb
@@ -0,0 +1,3 @@
+module GithubWhitespaceDemo
+  VERSION = "0.0.1"
+end
